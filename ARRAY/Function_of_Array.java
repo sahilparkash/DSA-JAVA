b169d37c1ea354ee52891cef27435b4de5f11339
sahil prakash <rjrajsing654@gmail.com>
2026-06-09 18:46:58 +0530

    insertion done

--- ARRAY/Function_of_Array.java
+++ ARRAY/Function_of_Array.java
@@ -24,12 +24,11 @@ class Function {
         }
         System.out.println();
     }
-
-    int Insert_at_First(int size , int[] arr) {
+    void Insert_at_First(int size , int[] arr) {
         this.size = size;
         if (size <= 0) {
             System.out.println("Please fill creat");
-            // Creat(size, arr);
+            Creat(size, arr);
         } else {
             int[] temp = new int[size + 1];
             System.out.println("Enter number to insert at first:");
@@ -42,31 +41,76 @@ class Function {
                 System.out.print(" " + temp[i]);
             }
             System.out.println();
-            return size + 1;
         }
-        return 0;
+    }
+    void Insert_at_Last(int size , int[] arr) {
+        if (size <= 0) {
+            System.out.println("Please fill creat");
+            Creat(size, arr);
+        } else {
+            int[] temp = new int[size + 1];
+            for (i = 0; i < size; i++) {
+                temp [i] = arr[i];
+            }
+            System.out.println("Enter number to insert at last:");
+            temp[size] = sc.nextInt();
+            System.out.print("arrays are:");
+            for (i = 0; i < temp.length; i++) {
+                System.out.print(" " + temp[i]);
+            }
+            System.out.println();
+        }
+    }
+    void Insert_at_Anypotion(int size , int[] arr) {
+        if (size <= 0) {
+            System.out.println("Please fill creat");
+            Creat(size, arr);
+        } else {
+            int[] temp = new int[size + 1];
+            System.out.println("Enter index to insert at Anypotion:");
+            int position = sc.nextInt();
+            temp[position] = sc.nextInt();
+            for (i = 0; i < size; i++) {
+                temp [i/position] = arr[i/position];
+            }
+                for(i=position;i<size;i++)
+                {
+                        temp[i+1]=arr[i];
+                }
+            System.out.print("arrays are:");
+            for (i = 0; i < temp.length; i++) {
+                System.out.print(" " + temp[i]);
+            }
+            System.out.println();
+        }
     }
 }
-
 public class Function_of_Array {
     public static void main(String[] args) {
         Function F1 = new Function();
         System.out.println("Enter size of array:");
         int size = new Scanner(System.in).nextInt();
         int[] arr = new int[size];    
-        System.out.println("1.To  Enter element in arrya"+" 2.TO Insert_at_First"+" 3.To Insert_at_Last"+" 4.Insert_at_Anypotion");
-        System.out.println("5.TO Delete_at_First"+" 6.To Delete_at_Last"+" 7.Delete_at_Anypotion"+" 0.to stop");
         boolean option=true ;
         while(option)
         {
+            System.out.println("1.To  Enter element in arrya"+" 2.TO Insert_at_First"+" 3.To Insert_at_Last"+" 4.Insert_at_Anypotion");
+            System.out.println("5.TO Delete_at_First"+" 6.To Delete_at_Last"+" 7.Delete_at_Anypotion"+" 0.to stop");
             int n =new Scanner (System.in).nextInt();
             switch ( n ) {
                 case 1 -> F1.Creat(size, arr);
                 case 2 -> F1.Insert_at_First(size, arr);
+                case 3 -> F1.Insert_at_Last(size, arr);
+                case 4 -> F1.Insert_at_Anypotion(size, arr);
+                // case 5 -> F1.Delete_at_First(size, arr);
+                // case 6 -> F1.Delete_at_Last(size, arr);
+                // case 7 -> F1.Delete_at_Anypotion(size, arr);
+                
+                
                 case 0 ->
                 {
+                    System.out.println("Thanks for using.");
                     option = false;
-                    n = 0;
                 } 
 
                 default -> 
--- ARRAY/tempCodeRunnerFile.java
+++ ARRAY/tempCodeRunnerFile.java
@@ -1,1 +1,4 @@
-}
+for(i=position;i<size;i++)
+                // {
+                //         temp[i+1]=arr[i];
+                // }
