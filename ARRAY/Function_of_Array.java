ad748e7dd923a1198c95daec8b0e054d3baea966
sahil prakash <rjrajsing654@gmail.com>
2026-06-11 15:51:53 +0530

    this makes whole done but this only copping to new arry then show this dsa method is not purle right but this can teach bacis

--- ARRAY/Function_of_Array.java
+++ ARRAY/Function_of_Array.java
@@ -24,13 +24,12 @@ class Function {
         }
         System.out.println();
     }
-    void Insert_at_First(int size , int[] arr) {
-        this.size = size;
+    int[] Insert_at_First(int size , int[] arr) {
+        int[] temp = new int[size + 1];
         if (size <= 0) {
             System.out.println("Please fill creat");
             Creat(size, arr);
         } else {
-            int[] temp = new int[size + 1];
             System.out.println("Enter number to insert at first:");
             temp[0] = sc.nextInt();
             for (i = 0; i < size; i++) {
@@ -42,13 +41,14 @@ class Function {
             }
             System.out.println();
         }
+        return temp;
     }
-    void Insert_at_Last(int size , int[] arr) {
+    int[] Insert_at_Last(int size , int[] arr) {
+        int[] temp = new int[size + 1];
         if (size <= 0) {
             System.out.println("Please fill creat");
             Creat(size, arr);
         } else {
-            int[] temp = new int[size + 1];
             for (i = 0; i < size; i++) {
                 temp [i] = arr[i];
             }
@@ -60,18 +60,19 @@ class Function {
             }
             System.out.println();
         }
+        return temp;
     }
-    void Insert_at_Anypotion(int size , int[] arr) {
+    int[] Insert_at_Anypotion(int size , int[] arr) {
+        int[] temp = new int[size + 1];
         if (size <= 0) {
             System.out.println("Please fill creat");
             Creat(size, arr);
         } else {
-            int[] temp = new int[size + 1];
             System.out.println("Enter index to insert at Anypotion:");
             int position = sc.nextInt();
             temp[position] = sc.nextInt();
-            for (i = 0; i < size; i++) {
-                temp [i/position] = arr[i/position];
+            for (i = 0; i < position; i++) {
+                temp [i] = arr[i];
             }
                 for(i=position;i<size;i++)
                 {
@@ -83,7 +84,67 @@ class Function {
             }
             System.out.println();
         }
+        return temp;
     }
+    int[] Delete_at_First(int size , int[] arr) {
+        int[] temp = new int[size - 1];
+        if (size <= 0) {
+            System.out.println("Please fill creat");
+            Creat(size, arr);
+        } else {
+            System.out.println("Number  at first Deleted:");
+            for (i = 1; i < size; i++) {
+                temp[i-1] = arr[i];
+            }
+            System.out.print("arrays are:");
+            for (i = 0; i < temp.length; i++) {
+                System.out.print(" " + temp[i]);
+            }
+            System.out.println();
+        }
+        return temp;
+    }
+    int[] Delete_at_Last(int size , int[] arr) {
+        int[] temp = new int[size - 1];
+        if (size <= 0) {
+            System.out.println("Please fill creat");
+            Creat(size, arr);
+        } else {
+            System.out.println("Number at last Deleted:");
+            for (i = 0; i < size-1; i++) {
+                temp [i] = arr[i];
+            }
+            for (i = 0; i < temp.length; i++) {
+                System.out.print(" " + temp[i]);
+            }
+            System.out.println();
+        }
+        return temp;
+    }
+    int[] Delete_at_Anypotion(int size , int[] arr) {
+        int[] temp = new int[size - 1];
+        if (size <= 0) {
+            System.out.println("Please fill creat");
+            Creat(size, arr);
+        } else {
+            System.out.println("Enter index to Delete at Anypotion:");
+            int position = sc.nextInt();
+            for (i = 0; i <position; i++) {
+                temp [(i)] = arr[(i)];
+            }
+                for(i=position;i<size;i++)
+                {
+                        temp[i-1]=arr[i];
+                }
+            System.out.print("arrays are:");
+            for (i = 0; i < temp.length; i++) {
+                System.out.print(" " + temp[i]);
+            }
+            System.out.println();
+        }
+    return temp;
+    }
+
 }
 public class Function_of_Array {
     public static void main(String[] args) {
@@ -99,14 +160,36 @@ public class Function_of_Array {
             int n =new Scanner (System.in).nextInt();
             switch ( n ) {
                 case 1 -> F1.Creat(size, arr);
-                case 2 -> F1.Insert_at_First(size, arr);
-                case 3 -> F1.Insert_at_Last(size, arr);
-                case 4 -> F1.Insert_at_Anypotion(size, arr);
-                // case 5 -> F1.Delete_at_First(size, arr);
-                // case 6 -> F1.Delete_at_Last(size, arr);
-                // case 7 -> F1.Delete_at_Anypotion(size, arr);
-                
-                
+                case 2 -> 
+                {
+                    arr = F1.Insert_at_First(size, arr);
+                    size = arr.length;
+                }
+                case 3 -> 
+                {
+                    arr = F1.Insert_at_Last(size, arr);
+                    size = arr.length;
+                }
+                case 4 -> 
+                {
+                    arr = F1.Insert_at_Anypotion(size, arr);
+                    size = arr.length;
+                }
+                case 5 -> 
+                {
+                    arr = F1.Delete_at_First(size, arr);
+                    size = arr.length;
+                }
+                case 6 -> 
+                {
+                arr = F1.Delete_at_Last(size, arr);
+                size = arr.length;
+                }
+                case 7 -> 
+                {
+                    arr = F1.Delete_at_Anypotion(size, arr);
+                    size = arr.length;
+                }
                 case 0 ->
                 {
                     System.out.println("Thanks for using.");
